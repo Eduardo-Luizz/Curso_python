19eeea2f0c8eb26a7d96f5030261e34a95c8b8b8
Eduardo Luiz Zanotto <91149760+Eduardo-Luizz@users.noreply.github.com>
2023-04-04 13:07:03 -0300

    Interactive helping system

--- modulo 4-2 funções parte 2/ex_106_interactive_helping_system.py
+++ modulo 4-2 funções parte 2/ex_106_interactive_helping_system.py
@@ -2,3 +2,33 @@
 # O usuário vai digitar o comando e o manual vai aparecer.
 # Quando o usuário digitar a palavra ‘FIM’,
 # o programa se encerrará. Importante: use cores.
+from time import sleep
+c = ('\033[m', '\033[0;30;41m', '\033[0;30;42m', '\033[0;30;43m', '\033[0;30;44m', '\033[0;30;45m', '\033[7;30m')
+
+
+def ajuda(com):
+    titulo(f'Acessando o comando \'{com}\'', 4)
+    print(c[5], end='')
+    help(com)
+    print(c[0], end='')
+    sleep(2)
+
+
+def titulo(msg, cor = 0):
+    tam = len(msg) + 4
+    print(c[cor], end='')
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+    print(c[0], end='')
+    sleep(1)
+
+comando = ''
+while True:
+    titulo('SISTEMA DE AJUDA PYHELP', 2)
+    comando = str(input("Função ou biblioteca > "))
+    if comando.upper() == 'FIM':
+        break
+    else:
+        ajuda(comando)
+titulo('ATÉ LOGO', 1)
